15ace31f8f67b57ffb5a8aaa8cb845c3c80c8563
Ansal S <ansal@bssatech.com>
2019-03-24 10:17:53 +0530

    initial socket setup for tcpserver

--- httpserver/tcpserver.py
+++ httpserver/tcpserver.py
@@ -5,13 +5,16 @@ import socket
 
 class TCPServer:
 
-    def __init__(self, ip, port, max_connections=5, bytes_count=1024):
+    def __init__(self, ip, port, callback=None, max_connections=5, bytes_count=1024):
 
         # The IP address this server binds to
         self.ip = ip
 
         # The port this server binds to
         self.port = port
+
+        # Callback to run after a request is served
+        self.callback = callback
 
         # Number of bytes to be read from the socket
         self.bytes_count = bytes_count
@@ -23,5 +26,17 @@ class TCPServer:
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def start(self):
+        # Set it as a non-blocking one
+        self._socket.setblocking(0)
+
+        # Bind the socket to specified ip and port
+        self._socket.bind((self.ip, self.port))
+
+        # And finally, start listening
         self._socket.listen(self._max_connections)
+
+
+if __name__ == '__main__':
+    server = TCPServer('127.0.0.1', 3000)
+    server.start()
 
